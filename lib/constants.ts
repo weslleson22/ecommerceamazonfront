8f5cb76926d61036a31ce51ef4419ae27055aa0d
weslleson22 <wesllesonsowza@gmail.com>
2025-04-12 10:30:30 -0300

    descrição +zoom

--- lib/constants.ts
+++ lib/constants.ts
@@ -4,3 +4,5 @@ process.env.NEXT_PUBLIC_APP_SLOGAN || 'Spend less, enjoy more'
 
 export const APP_DESCIPRION = 
 process.env.NEXT_PUBLIC_APP_DESCIPRION || 'An Amazon Clone With Next.js 13 and Mongo DB' 
+
+export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9)
